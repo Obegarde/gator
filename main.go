6af300a88372dec4c35f75194118d60291b553f2
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-10-25 17:43:26 +0200

    Add resetting users and registering users

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ import(
 	"fmt"
 	"github.com/obegarde/gator/internal/config"
 	"os"
+	"database/sql"
+	"github.com/lib/pq"
+	"github.com/obegarde/gator/internal/database"
+	"context"
+	"github.com/google/uuid"	
+	"time"
+	
 )
+
 
 type state struct{
 	config *config.Config
+	db *database.Queries
 }
 
 type command struct{
@@ -26,11 +35,21 @@ func main(){
 	}
 	theState := state{}
 	theState.config = &configData
+	db, err := sql.Open("postgres",theState.config.DbURL)
+	if err != nil{
+		fmt.Printf("SqlError: %v\n", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+	theState.db = dbQueries
 	theCommands := newCommands()
 	theCommands.register("login", handlerLogin)
+	theCommands.register("register", handlerRegister)
+	theCommands.register("reset",handlerResetUsers)
 	if len(os.Args) < 2{
 		os.Exit(1)
 	}
+	
 	theCommand := new(command)
 	theCommand.name = os.Args[1]
 	theCommand.args = os.Args[2:]
@@ -39,13 +58,54 @@ func main(){
 	fmt.Printf("run Error: %v\n", err)
 	os.Exit(1)
 	}
+
+}
+func handlerResetUsers(s *state, _ command) error{
+	fmt.Println("Attempting to reset users")
+	err := s.db.ResetUsers(context.Background())
+	if err != nil{
+	return err
+	}
+	return nil
+}
+
+
+func handlerRegister(s *state, cmd command) error{
+	if len(cmd.args) < 1{
+	return fmt.Errorf("Too few arguements, please pass in a name together with the register command")
+	}
+	newUserParams := new(database.CreateUserParams)
+	newUserContext := context.Background()
+	newUserParams.ID = uuid.New()
+	newUserParams.Name = cmd.args[0]
+	newUserParams.CreatedAt = time.Now()
+	newUserParams.UpdatedAt = time.Now()
+	fmt.Printf("Attempting to create user with name: %s\n", newUserParams.Name)
+	newUser,err := s.db.CreateUser(newUserContext, *newUserParams)
+	if err != nil{
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505"{
+			return fmt.Errorf("user already exists")	
+		}
+	return err
+	}
+	err = config.SetUser(newUserParams.Name, *s.config)
+	if err != nil{
+	return err
+	}
+	fmt.Printf("User successfully registered\nUserData: %v\n",newUser)
+	return nil
 }
 
 func handlerLogin(s *state, cmd command) error{
 	if len(cmd.args) == 0{
 		return fmt.Errorf("Error: Expected 1 arg, a username") 
 	}
-	err := config.SetUser(cmd.args[0],*s.config)
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err != nil{
+	os.Exit(1)
+	}
+
+	err = config.SetUser(cmd.args[0],*s.config)
 	if err != nil{
 	return fmt.Errorf("handlerLogin Error: %w\n", err)
 	}
